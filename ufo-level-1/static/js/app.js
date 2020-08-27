9f1f7fabf9522e9e0cde37d0e4546aee83c08eab
John Shows <johnshows@Johns-MacBook-Pro-4.local>
2020-08-27 14:03:11 -0400

    automatic table commit

--- ufo-level-1/static/js/app.js
+++ ufo-level-1/static/js/app.js
@@ -1,4 +1,38 @@
-// from data.js
-var tableData = data;
+//from data.js
+// var tableData = data;
 
 // YOUR CODE HERE!
+
+
+var tbody = d3.select("tbody");
+
+
+//////// arrow function notation///////
+
+// var tbody = d3.select('tbody');
+// // Using forEach loop to add new data to the table
+// tableData.forEach((data) => {
+//     var row = tbody.append('tr');
+//     Object.entries(data).forEach(([key, value]) => {
+//         var cell = row.append('td');
+//         cell.text(value);
+//     });
+// });
+
+
+
+/// naming function manually ////
+
+console.log(data);
+
+data.forEach(function(tabledata){
+    console.log(tabledata);
+
+    var row = tbody.append("tr");
+    Object.entries(tabledata).forEach(function([key,value]){
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+
